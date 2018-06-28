cfcbd44876ffb4048c995ee5cafc7b20fdcf5e90
maximusman553 <192118@mail.ru>
2018-06-29 00:10:37 +0300

    findsoemebugs

---
+++ chat/core/static/core/js/chat.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    var form = $(".input");
+    form.on('submit', function (e) {
+        e.preventDefault();
+
+        const formData = new FormData(this);
+
+        $.ajax({
+            type: 'POST',
+            url: '/chat/message_create/',
+            data: formData,
+            processData: false,
+            contentType: false,
+
+            success: (result) => {
+                $('.chat').prepend(result['rendered_template']);
+                $('#message_input').val("");
+            }
+        });
+    });
+
+    setInterval(function () {
+        const lastId = $('.message').first().data("id");
+
+            $.ajax({
+            type: 'GET',
+            url: '/chat/messages/',
+            data: {'last_id': lastId},
+
+
+            success: (result) => {
+                if (result !== ""){
+                    $('.chat').prepend(result);
+                }
+            }
+        });
+
+    }, 2*1000);
+});
